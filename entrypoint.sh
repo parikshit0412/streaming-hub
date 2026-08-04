6a04d7511ee559950399f708c2850323150af20c
Parikshit Das <45668288+parikshit0412@users.noreply.github.com>
2026-08-05 04:06:51 +0530

    fix: create default /etc/grafana/grafana.ini in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Ensure Grafana config directory and file exist
+mkdir -p /etc/grafana
+touch /etc/grafana/grafana.ini
+
 # Start Grafana service in background on port 3000
 grafana-server --homepath /usr/share/grafana --config /etc/grafana/grafana.ini &
 
